4d8d9c6b9a2ec39eebe9e1bc521c88f01d691182
Babusha Kolhe <165826953+vickykolhe@users.noreply.github.com>
2025-08-07 10:27:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,6 +4,27 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
+	const priceElements = document.querySelectorAll('.price');
+    let total = 0;
+
+	priceElements.forEach(priceEl => {
+        const value = parseFloat(priceEl.textContent);
+        if (!isNaN(value)) {
+            total += value;
+        }
+    });
+
+	 const table = document.getElementById('gtable');
+	let tabletr=document.createElement("tr");
+	let tabletd=document.createElement("td");
+
+	tabletd.setAttribute('colspan', 2);
+	tabletd.textContent=total;
+
+	tabletr.appendChild(tabletd);
+	table.appendChild(tabletr);
+	
+	
   
 };
 
